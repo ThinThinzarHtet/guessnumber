24db5757831f9a6b13c1a41ebf6361c055fe32b3
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2021-12-23 21:11:59 +0630

    add highscore feature

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number 👍🏽';
 
-// document.querySelector('.number').textContent = '13';
-// document.querySelector('.score').textContent = '10';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-// document.querySelector('.guess').value = 13
-// console.log(document.querySelector('.guess').value);
-
-// const x = function() {
-//     console.log(document.querySelector('.guess').value);
-// }
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 let score = 20;
 
+let highScore = 0;
+
+// when click check button
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
 
@@ -25,8 +16,16 @@ document.querySelector('.check').addEventListener('click', function() {
         // When player win
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number 👍🏽';
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = 'green';
         document.querySelector('.number').style.width = '20rem';
+
+        // highscore   
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+        
         // When guess is too high
     } else if (guess > secretNumber) { 
         if (score > 0) {
@@ -48,4 +47,17 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = '0';
         }
     } 
+});
+
+// when click again button
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
